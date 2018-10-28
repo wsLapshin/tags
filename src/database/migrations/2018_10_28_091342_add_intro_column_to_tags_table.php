6eedff93b5971cdcc839df2601a8f3bd96fa9151
Thomas Venturini <thomas@venturini.codes>
2018-10-28 11:32:19 +0100

    add columns intro, image, slug

--- src/database/migrations/2018_10_28_091342_add_intro_column_to_tags_table.php
+++ src/database/migrations/2018_10_28_091342_add_intro_column_to_tags_table.php
@@ -1,9 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTagsTable extends Migration
+class AddIntroColumnToTagsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,10 +13,8 @@ class CreateTagsTable extends Migration
      */
     public function up()
     {
-        Schema::create('tags', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name')->unique();
-            $table->timestamps();
+        Schema::table('tags', function (Blueprint $table) {
+            $table->text('intro')->nullable();
         });
     }
 
@@ -26,6 +25,8 @@ class CreateTagsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('tags');
+        Schema::table('tags', function (Blueprint $table) {
+            $table->dropColumn('intro');
+        });
     }
 }
